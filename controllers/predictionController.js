d683b0c5051ad20f7c137df03f8561827d38ba7e
chapi4443 <yamyabu35i@gmail.com>
2023-10-03 07:35:12 -0700

    4

--- controllers/predictionController.js
+++ controllers/predictionController.js
@@ -1,20 +1,55 @@
-const logisticRegressionModel = require("../models/logisticRegressionModel");
+// controllers/predictionController.js
+const axios = require("axios");
+const User = require("../models/User");
+const StrokePrediction = require("../models/StrokePrediction");
 
 async function predictStrokeRisk(req, res) {
   try {
-    const inputData = req.body; // Assuming the request contains the input data as JSON
+    const { userId } = req.user; // Assuming you have the user's ID available in req.user
 
-    if (!inputData) {
-      return res.status(400).json({ error: "Input data is missing." });
-    }
+    // Forward the request to the Flask application
+    const flaskUrl = "http://127.0.0.1:4000"; // Change the port if necessary
+    const response = await axios.post(
+      `${flaskUrl}/predict_stroke_risk`,
+      req.body
+    );
 
-    const probability = await logisticRegressionModel.predict(inputData);
+    // Store the prediction data in MongoDB
+    const predictionData = {
+      user: userId, // Link the prediction to the user
+      prediction: response.data["Logistic Regression Probability"],
+      data: req.body.data[0], // Save the input data used for the prediction
+    };
 
-    res.json({ probability });
+    const prediction = new StrokePrediction(predictionData);
+    await prediction.save();
+
+    // Send the Flask application's response back to the client
+    res.json(response.data);
   } catch (error) {
-    console.error("Prediction error:", error);
-    res.status(500).json({ error: "Internal server error" });
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 }
 
-module.exports = { predictStrokeRisk };
+
+
+// controllers/predictionController.js
+
+async function getAllPredictions(req, res) {
+  try {
+    // Fetch all predictions from the database
+    const predictions = await StrokePrediction.find();
+
+    // Send the predictions as a JSON response
+    res.json({ predictions });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+module.exports = {
+  predictStrokeRisk,
+  getAllPredictions, // Add the new controller function
+};
--- routes/flaskRoute.js
+++ routes/flaskRoute.js
@@ -1,24 +1,39 @@
-const express = require("express");
-const axios = require("axios");
+// const express = require("express");
+// const axios = require("axios");
+// const router = express.Router();
+
+// router.post("/predict_stroke_risk", async (req, res) => {
+//   try {
+//     // Replace with the actual URL where your Flask application is running on port 4000
+//     const flaskUrl = "http://127.0.0.1:4000"; // Change the port to 4000
+
+//     // Forward the request to the Flask application
+//     const response = await axios.post(
+//       `${flaskUrl}/predict_stroke_risk`,
+//       req.body
+//     );
+
+//     // Send the Flask application's response back to the client
+//     res.json(response.data);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+// module.exports = router;
+////////////////////////////////////////////////////////////////////
+// routes/predictionRoutes.js
+const express = require('express');
 const router = express.Router();
+const predictionController = require('../controllers/predictionController');
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
 
-router.post("/predict_stroke_risk", async (req, res) => {
-  try {
-    // Replace with the actual URL where your Flask application is running on port 4000
-    const flaskUrl = "http://127.0.0.1:4000"; // Change the port to 4000
-
-    // Forward the request to the Flask application
-    const response = await axios.post(
-      `${flaskUrl}/predict_stroke_risk`,
-      req.body
-    );
-
-    // Send the Flask application's response back to the client
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+// Define the route for predicting stroke risk
+router.post('/predict_stroke_risk',authenticateUser, predictionController.predictStrokeRisk);
+router.get("/predictions",authenticateUser, predictionController.getAllPredictions);
 
 module.exports = router;
